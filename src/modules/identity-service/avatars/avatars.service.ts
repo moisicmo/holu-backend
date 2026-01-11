e22a2daa07b2e87875df620b9cbddd7a9a6935bc
moises <moisic.mo@gmail.com>
2026-01-11 00:30:38 -0400

    feat: ajuste de folder avatars

--- src/modules/identity-service/avatars/avatars.service.ts
+++ src/modules/identity-service/avatars/avatars.service.ts
@@ -5,14 +5,13 @@ import { join } from 'path';
 @Injectable()
 export class AvatarsService {
   findAll(): string[] {
-    const avatarsDir = join(__dirname, '..', '..', '..', '..', 'public', 'avatars');
-    const baseUrl = 'https://holu-backend-production.up.railway.app/static/avatars';
+    const avatarsDir = join(__dirname, '..', '..', '..', 'public', 'avatars');
 
     const files = readdirSync(avatarsDir);
 
     // Opcional: filtrar solo imágenes .png o .jpg
     const images = files.filter(file => file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.webp'));
 
-    return images.map(file => `${baseUrl}/${file}`);
+    return images;
   }
 }
